154c574e7ed072bb36fcdc1ec392b2ece730a17e
Troy Pavlek <troy@tpavlek.me>
2017-01-16 13:45:34 -0700

    pulls children

--- app/Model/OpenGraphRetriever.php
+++ app/Model/OpenGraphRetriever.php
@@ -2,6 +2,8 @@
 
 namespace App\Model;
 
+use Illuminate\Support\Collection;
+
 class OpenGraphRetriever
 {
 
@@ -12,7 +14,20 @@ class OpenGraphRetriever
 
     public static function getComments(NewsArticle $article)
     {
-        return app('facebook')->get($article->og_id . "/comments?limit=100")->getDecodedBody()['data'];
+        $comments = new Collection();
+        $response = app('facebook')->get($article->og_id . "/?fields=comments{message,from,comments}&limit=100")->getDecodedBody()['comments']['data'];
+
+        collect($response)->each(function ($comment) use ($comments) {
+            $comments->push(array_only($comment, [ 'message', 'from', 'id' ]));
+
+            if (isset($comment['comments'])) {
+                collect($comment['comments']['data'])->each(function ($comment) use ($comments) {
+                    $comments->push(array_only($comment, [ 'message', 'from', 'id' ]));
+                });
+            }
+        });
+
+        return $comments->all();
     }
 
 }
